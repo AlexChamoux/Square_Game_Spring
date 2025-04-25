8679e3c26609ebc62cd641932e61e7a1646ffffd
AlexChamoux <alex.chamoux38@gmail.com>
2025-04-25 10:26:58 +0200

    add project to my personal GitHub

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+@Entity
+@Table(name = "game_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+}
